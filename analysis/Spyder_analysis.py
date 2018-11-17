41b6584a9ab9402c5af021e55bf21a9b39b68c40
Kayanski <kowalski.kowalskin@gmail.com>
2018-11-17 16:01:17 +0100

    Ajout d'un fichier de test pour Spyder, avec animation

--- analysis/Spyder_analysis.py
+++ analysis/Spyder_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+'''
 # make sure labels are large enough in report
 import matplotlib as mpl
 mpl.rcParams['figure.figsize'] = (10,8)
@@ -12,27 +12,28 @@ mpl.rcParams['lines.markersize'] = 10.0
 
 
 #temp_range = np.hstack([np.arange(0.5,2.,0.5), np.arange(2.,2.5,0.05), np.arange(2.5,5,0.5)])
-
+'''
 L = 10
 
 pos = np.loadtxt("../results/position.dat")
 vel = np.loadtxt("../results/speed.dat")
 
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=30, bitrate=1800)
 #pos = np.random.rand(1000,100)
 #vel = np.random.rand(1000,100)
 
 fig = plt.figure()
 #plt.legend()
-ax = fig.add_subplot(111)
+ax = plt.axes(xlim=(0, 1000), ylim=(0,1000))
 line, = ax.plot([], [], 'bo', ms=5)
-ax.set_xlim(0, 1)
-ax.set_ylim(-1,1)
 
 def make_frame(t):
-    X = pos[(t%1000),:]
-    V = vel[(t%1000),:]
+    X = t
+    V = t
     line.set_data(X, V)
     return line,
 
-ap = animation.FuncAnimation(fig, make_frame, interval=1,blit=True)
-plt.show()
+ap = animation.FuncAnimation(fig, make_frame, frames = 1000,interval=33,blit=True)
+#plt.show()
+ap.save("ouioui.mp4",writer=writer)
